2a25dc9bf77ee0383bbbe519398d7c54595669ca
Damandeep Singh <damandeep.in@gmail.com>
2025-07-12 14:45:25 +0530

    updated spaced and slate skin

--- src/js/videojs-skin.js
+++ src/js/videojs-skin.js
@@ -16,6 +16,17 @@ import videojs from "video.js";
         player.addClass('skin_spaced')
         player.controlBar.removeChild('VolumePanel')
         player.controlBar.addChild('VolumePanel',{inline:false},2)
+        const ProgressControl=this.controlBar.getChild('ProgressControl');
+        const CurrentTimeDisplay=this.controlBar.getChild('CurrentTimeDisplay');
+        const customProgressBar=document.createElement('div');
+        customProgressBar.className='custom-spaced-progress-bar'
+        customProgressBar.append(ProgressControl.el());
+        customProgressBar.append(CurrentTimeDisplay.el());
+
+        const SeekToLive=this.controlBar.getChild('SeekToLive');
+        customProgressBar.append(SeekToLive.el());
+        this.controlBar.el().append(customProgressBar);
+        
         break;
       default :
         player.addClass('skin_slate')
